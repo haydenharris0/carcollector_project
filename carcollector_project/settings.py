24a6a8c9bd38655ab15aa9843238ff6b12c0d243
Hayden Harris <haydenharris0@gmail.com>
2021-04-26 13:10:47 -0500

    debugging issues

--- carcollector_project/settings.py
+++ carcollector_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://carcollector-project.herokuapp.com/']
 
 
 # Application definition
